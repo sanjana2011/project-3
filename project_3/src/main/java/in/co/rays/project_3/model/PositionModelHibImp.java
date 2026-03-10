3b3289b796d4793d510671bc882935786f7b1007
Sanjana Gangrade <87362088+sanjana2011@users.noreply.github.com>
2026-03-10 14:18:54 +0530

    Fix import formatting in PositionModelHibImp.java

--- project_3/src/main/java/in/co/rays/project_3/model/PositionModelHibImp.java
+++ project_3/src/main/java/in/co/rays/project_3/model/PositionModelHibImp.java
@@ -3,7 +3,7 @@ package in.co.rays.project_3.model;
 import java.util.List;
 
 import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
+import org.hibernate.HibernateException; 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
