849963ec1d043259096301dc5f0f2ececeb04dbe
Sanjana Gangrade <87362088+sanjana2011@users.noreply.github.com>
2026-03-10 14:18:42 +0530

    Fix import formatting in PositionModelInt.java

--- project_3/src/main/java/in/co/rays/project_3/model/PositionModelInt.java
+++ project_3/src/main/java/in/co/rays/project_3/model/PositionModelInt.java
@@ -1,6 +1,6 @@
 package in.co.rays.project_3.model;
 
-import java.util.List;
+import java.util.List; 
 
 import in.co.rays.project_3.dto.PositionDTO;
 import in.co.rays.project_3.exception.ApplicationException;
