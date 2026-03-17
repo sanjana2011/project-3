8973db6a953f4c3b9f541b80f5a7d2c828d55ef7
Sanjana Gangrade <87362088+sanjana2011@users.noreply.github.com>
2026-03-17 14:16:31 +0530

    Fix import formatting for HttpServletResponse

--- project_3/src/main/java/in/co/rays/project_3/controller/PositionListCtl.java
+++ project_3/src/main/java/in/co/rays/project_3/controller/PositionListCtl.java
@@ -8,7 +8,7 @@ import java.util.List ;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse;  
 
 import in.co.rays.project_3.dto.BaseDTO;
 import in.co.rays.project_3.dto.PositionDTO;
